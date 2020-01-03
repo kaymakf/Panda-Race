f182eca0d5874faebb0a8d3f3cf992aad15c075e
Furkan <f.kaymakk@gmail.com>
2020-01-03 16:22:00 +0300

    #2 guest login

--- Assets/Scripts/Login/Auth.cs
+++ Assets/Scripts/Login/Auth.cs
@@ -1,12 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using Amazon.CognitoIdentity;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Auth : MonoBehaviour
-{
-    public void GuestLogin()
-    {
-        SceneManager.LoadScene("MainMenu");
-    }
+public class Auth : MonoBehaviour {
+	public GameObject LoadingCircle;
+	private Action LoginSuccess = () => SceneManager.LoadScene("MainMenu");
+	private Action<GameObject> LoginFailed = (loading) => loading.SetActive(false);
+
+	void Awake() {
+		if (WebServicesManager.I == null)
+			SceneManager.LoadScene("Singleton");
+	}
+
+	public void OnGuestLoginClick() {
+		LoadingCircle.SetActive(true);
+		WebServicesManager.I.Credentials.GetIdentityIdAsync(Login);
+	}
+
+	void Login(AmazonCognitoIdentityResult<string> result) {
+		if (result.Exception != null) {
+			Debug.Log("Login failed.");
+			return;
+		}
+
+		string identityId = result.Response;
+		Debug.Log("Login successful: " + identityId);
+
+		WebServicesManager.I.LoadStateAsync(LoginSuccess, LoginFailed, LoadingCircle);
+	}
 }
---
+++ Assets/Scripts/Model/PlayerModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModel {
+	public string Id;
+	public string Name;
+}
