1d6b03df6b72f822a406c27c430a9190b33fad4c
Kurbağa <33623248+EzgiVural@users.noreply.github.com>
2020-01-03 16:04:43 +0300

    #5 Menü tasarımı yapıldı. Options eklendi. Game scene oluşturuldu. Tilemap eklendi.

--- Assets/Scripts/MainMenu/MainMenuButtons.cs
+++ Assets/Scripts/MainMenu/MainMenuButtons.cs
@@ -1,11 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour
 {
-    public void Logout()
+    public GameObject OptionsMenuObject;
+
+    void Start()
     {
         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnOptions()
+    {
+        OptionsMenuObject.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void OnLogOut()
+    {
+        SceneManager.LoadScene("Login");
+
+    }
+
+    public void OnPlay()
+    {
+        SceneManager.LoadScene("Game");
     }
 }
